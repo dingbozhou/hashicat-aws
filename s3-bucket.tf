5e724ef018597b149600b68c897f605ff93857ca
root <root@p-bxwgtn1m3rim-workstation.c.instruqt-prod.internal>
2021-01-23 17:38:22 +0000

    test

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = var.bucket
+
   acl    = "private"
-  bucket_prefix = var.bucket_prefix
+ 
 
   versioning = {
     enabled = true
